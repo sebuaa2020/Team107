315bebf3b6a43c1db47924203c7d0e2c1ffac643
Cherish <36319110+yang-jiacheng@users.noreply.github.com>
2020-05-20 09:35:07 +0800

    Update navigation_interface.cpp

--- Team107_ws/src/vel_pkg/src/navigation_interface.cpp
+++ Team107_ws/src/vel_pkg/src/navigation_interface.cpp
@@ -6,30 +6,34 @@
  move_base在world中的目标
 */ 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>  MoveBaseClient;
+
 int main(int argc, char** argv) {
-ros::init(argc, argv, "navigation_interface_node");
-MoveBaseClient ac("move_base", true);
-// Wait 60 seconds for the action server to become available
-ROS_INFO("Waiting for the move_base action server");
-ac.waitForServer(ros::Duration(60));
-ROS_INFO("Connected to move base server");
-// Send a goal to move_base
-//目标的属性设置
-move_base_msgs::MoveBaseGoal goal;
-goal.target_pose.header.frame_id = "map";
-goal.target_pose.header.stamp = ros::Time::now(); //设置导航点
-double x,y;
-std::cin >> x >> y;
-goal.target_pose.pose.position.x = x;
-goal.target_pose.pose.position.y = y;
-goal.target_pose.pose.orientation.w = 1;
-ROS_INFO("Sending goal");
-ac.sendGoal(goal);
-// Wait for the action to return
-ac.waitForResult();
-if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-ROS_INFO("You have reached the goal!");
-else
-ROS_INFO("The base failed for some reason");
-return 0;
+    ros::init(argc, argv, "navigation_interface_node");
+    MoveBaseClient ac("move_base", true);
+ 
+    // Wait 60 seconds for the action server to become available
+    ROS_INFO("Waiting for the move_base action server");
+    ac.waitForServer(ros::Duration(60));
+    ROS_INFO("Connected to move base server");
+ 
+    // Send a goal to move_base
+    //目标的属性设置
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now(); //设置导航点
+    double x,y;
+    std::cin >> x >> y;
+    goal.target_pose.pose.position.x = x;
+    goal.target_pose.pose.position.y = y;
+    goal.target_pose.pose.orientation.w = 1;
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+ 
+    // Wait for the action to return
+    ac.waitForResult();
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("You have reached the goal!");
+    else
+    ROS_INFO("The base failed for some reason");
+    return 0;
 }

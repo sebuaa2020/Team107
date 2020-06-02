46742276d69e2fc668c89a576fa9ed07335d00e2
leezg <2655421818@qq.com>
2020-06-02 20:56:36 +0800

    解决地图中机器人运动问题

---
+++ Android Part/appforros/app/src/main/java/com/example/appforros/RobotView.java
@@ -0,0 +1,42 @@
+package com.example.appforros;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Base64;
+import android.view.View;
+
+public class RobotView extends View {
+    private float bitmapX;
+    private float bitmapY;
+
+    public RobotView(Context context) {
+        super(context);
+        bitmapX = 160;
+        bitmapY = 160;
+    }
+
+    public void setBitmapX(int x) {
+        bitmapX = (float) x;
+    }
+
+    public void setBitmapY(int y) {
+        bitmapY = (float) y;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        Bitmap bitmap = Bitmap.createBitmap(5,5,Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.parseColor("#ff0000"));
+        canvas.drawBitmap(bitmap, bitmapX, bitmapY, paint);
+        System.out.println("bitmap" + bitmap.toString());
+        if (bitmap.isRecycled()) {
+            bitmap.recycle();
+        }
+    }
+}
--- Android Part/appforros/app/src/main/java/com/example/appforros/ui/plan/PlanFragment.java
+++ Android Part/appforros/app/src/main/java/com/example/appforros/ui/plan/PlanFragment.java
@@ -29,6 +29,7 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.appforros.R;
 import com.example.appforros.Robot;
 import com.example.appforros.RobotList;
+import com.example.appforros.RobotView;
 import com.example.appforros.User;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -46,6 +47,7 @@ public class PlanFragment extends Fragment {
     private FrameLayout.LayoutParams robot_margin;
     private int map_width = 992;
     private int map_height = 992;
+    private RobotView robotView;
     //private String test_image = "iVBORw0KGgoAAAANSUhEUgAAA+AAAAPgCAAAAACXTEmhAAAQd0lEQVR4nO3dSXbbSBZAUWUd74n7H3JVWYNMV1mNZQKBJvBw78CW3IAY+OEHGtJ//f0GVP3n7B0A9iNwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmE/zt4BLun5z0+Pc/eCPzLBIUzgECZwCBM4hAmcNVxduwiBM+B59g7wBwKHMIFDmMAhTODMzon+AIEzuacr9gMEzuxM8AECZ5XDxurTPfcRAmd2JvgAgTO1p6fmhgic2ZngAwTOzJ5vJvgQgTPiiOlqgg8QOBN7vr2Z4EMEzuxM8AECZ50j5qq0hwmcaf17i8wSfYDAmdnzzRwfInBm9fMWmQk+QODMzAQfJHAmpestCBzCBM6cDPBNCJyVXPu6AoEzZK9Ba4BvQ+AQJnBmZIBv5MfZOwCfPb/57s3p/wImOIQJnPlYoG9G4BAmcNba7UzYAN+OwCFM4MzGAN+QwCFM4EzGAN+SwBmjx6kJnLk4YGxK4ExF39sSOKt5JHx+AmcmBvjGBA5hAmciBvjWBA5hAmceBvjmBA5hAmcaBvj2BM56294I1/cOBM4gXc5M4EzCgWIPAocwgTOHBQPcM/CvEzhXo+8FBM4UXh/g+l5C4FyLvhcROAM2q+3lAa7vZQTOleh7IYEzgVcHuL6XEjjn0/duBM5l6Hs5gTNq+BnTFzeg7xUEzkXoew2Bc7bXBri+VxH4Dez4Pq3DstP3OgLve25U+HOfA8VLW9X3SgK/hau/11rfawn8Hjaa4ZtsZfk29b2awG9i1hmu730J/C42KXzWwwS/I/Db2Oka2ZgZ9ylF4DcyX03z7VGNwO9kfIh/2sDjq19kGgK/l7lanGtvkgR+M6NNbdmkvvcn8LsZXaar8lJ+nL0DHO65133lhfe0HSoOYILf0NgQ36pLfR/BBL+ljYf4glZ//lFPpx3DBL+nkSE+73PtfCLwuzo3sIe+jyHw21o/xN/9RWvtqQn8xk6cogb4QQR+Z2uHuDwvQ+B93y2iFR4n8Jub8k2kbEbgt7eqcIeFixB4359iNMTDBM6qeeygcA0C73vhTvVIrm6Ez8yz6BmD7yB5/P/L1/6GsK9A4Pxj4mCf7x9sfUy8q9OxROdfP6+1vVrPgWfhz2++41sCzxgfa/MWzloC73gMJ+6GWY7A+dXz7c0ILxE47zyfb+58hQicDxYU/s8IdziYmMBLtknNmXiIwPnsuWyEMy+B84WnZ0kiBM6XXizcCJ+cwFM2nLuvPpK+3SuyA4FDmMAZY4RPTeAMc0FuXgJvOaE1I3xmAocwgTPKCJ+YwBmm8HkJHMIEHuOKNr8SOIQJHMIEDmEChzCBQ5jAIUzgMR464VcChzCBQ5jAIUzgECbwFtfYeEfgECZwCBM4hAk8xSk47wkcwgQOYQKHMIGXOAXnA4FDmMAh7MfZO8BWrM/5zASHMIFDmCV6xsMqnU9McAgTOIQJvMT/PMgHAocwgUOYwCFM4ClOwnlP4BAmcAgTOIQJHMIE3uIqG+8IHMIEDmEChzCBxzgJ51cChzCBQ5jAIUzgECZwCBM4hAk8xicn8yuBQ5jAIUzgECbwFqfgvCNwCBM4hAkcwgQOYQJPcY2N9wQOYQKHsB9n7wCX8Hiz/r8kE7xktwR90ttVmeD8kbyvywTnT/R9YSZ4yC4rdHlfmsArdjr/1ve1CZxvyPvqnIPze2v7fjgwzMIEr/inqS0X6iOVPtw0n4PA+dpA3ub3PCzR+dJopCKfgwnOF4by1PZETHA+2yJRmU/BBOejwTSVPRMTnA8EWiJw3hm+hf349AUnEnjK8N3n4SplPReB83/bPoGm9Qm4yMZPrp0HmeD8S5tFAuft7W2r1fnjm+84gyU6b2+vtfjzz3gfyYWY4Gx3ce3jZozw0wkcHYZZot/ednk7UMzHBL+7XfuW/NlM8HuboMBvd8EFvUECv7P5H1x7SHyMJfqNHTC+x19igjXGlZngt7VxObuFaIiPMMHv6qDJ6AH3cwn8ngYebfl6nu4aocJXE/gtXa0Y/5PCWgK/oR1y+f0Wd3oKltcI/H6u2co19/p0Ar+bXVa7321zq9ezTF9D4PeyTyVDG32+fBdM4csJ/FbmTETh+xH4jezxqS1f/8qS335bUrjEFxL4fUwch2X6XgR+F/sNvz9u+JVXVvg+BH4T03fx8hC3TF9C4Cm/+7e/ZRQfQ9zsNpghvgOB38HZRbz0+k/vGtuBt4v27Zv3llt/nn4oyjHB63Y+Zd12668v03mNwOPmGImv7oVl+tYs0dN2z3v7F7BM35QJXjZPKgv2xDJ9SyZ41yU+U/GzpyG+IRM868KRGOKbEXjUIc97LVl4L9nuU+FbEXjTfnkfkt5zwdtP+I7Aiw56XHvRqyzdJUN8EwLv2TXvX7Lb9yhimb4Jgedc+OLaO5bpWxB4zM6r84EBvmLHDPFhAm/ZeXwfHJxl+rC//j57D7iOX3NbcShZV2vllOMcJjhzM8SHCJyXDQ7wlS/6cK1tgMB51XBn6w4KT0N8gMBZ49ATY9fa1hM4Kxx84csyfTWBc5zVxwXL9LUEznIn3LmyTF9H4Bxo4Mhgmb6KwFnsnEdPLNPXEDiT+nQYeTw91LaYR1V51c/5OZTZ74ewevdggkOYwFlobNKa08cSOIQJnGWM4EsROIsM9+0AcSiBQ5jAWWKD+WuEH0ngECZwFjB9r0bgHM1R4kAC53XSvByBQ5jAedlWA9xC4DgC51W6vCCBczyHisMIHMIEDmEC5wTW6EcROIQJnDMY4QcROHOQ/C4Ezhx85vkuBM4pDOxjCBzCBM45jPBDCBzCBA5hAuck1uhHEDiECRzCBM5ZrNEPIHAIEzinefz2G7YicKag730IHMIEznmM7d0JHMIEzoken75gWwJnBj7uYScChzCBc6bHh5/ZmMCZgSX6TgTOqYzufQkcwgQOYQLnXI9ffmRzAmcGLrLtROAQJnBO9vjfD2xP4MzAEn0nAudspveOBM4MRL4TgTMDS/SdCJzTGd/7ETgz0PhOBM755L0bgUPYX3+fvQfAbkxwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAgTOIQJHMIEDmEChzCBQ5jAIUzgECZwCBM4hAkcwgQOYQKHMIFDmMAhTOAQJnAIEziECRzCBA5hAocwgUOYwCFM4BAmcAj7L203DrqtAugDAAAAAElFTkSuQmCC";
     //private String test_image = "AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAQAQAAAAAAAAAAAAAAAAAAAAAAAB9SR//fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/6mHbP+LXDf/fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/31JH/99SR//i104/5lwT/+RZkP/fksh/6eFaP/8/Pv/mG9N/31JH/99SR//fUkf/31JH/99SR//fUkf/31JH/+tjHL/6uHb//7+/v////////////Xx7v/8+/r//////6N/Yv99SR//fUkf/31JH/99SR//fUkf/35LIf/PvK3///////////////////////////////////////////+vj3b/fUkf/31JH/99SR//fUkf/31JH/++pI/////////////08Oz/vqSQ/8y3p///////////////////////u6CK/31JH/99SR//fUkf/31JH/+IWDL/+vn3///////s5d//iVo1/6B7XP/6+ff/8Orl/9TDtv+5nYb/nXZX/4NRKf9+SyL/fUkf/31JH/99SR//sJF3////////////onxe/35LIv+ge1z/ils1/31JH/99SR//fUkf/6qIbf/dz8T/1MK0/31JH/99SR//fUkf/8WunP///////Pv7/39MIv99SR//fUkf/31JH/99SR//fUkf/31JH//ay7///////+ri2/99SR//fUkf/31JH//Frpv///////38+/9/TCP/fUkf/31JH/99SR//fUkf/31JH/99SR//2szA///////q4dv/fUkf/31JH/99SR//r491////////////pIBi/31JH/99SR//fUkf/31JH/99SR//hVUt//j29P//////1MK1/31JH/99SR//fUkf/4dXMP/59/b//////+7o4/+MXjn/fUkf/31JH/99SR//gE0k/9XFuP///////////6aDZ/99SR//fUkf/31JH/99SR//up+I////////////9vPw/8OrmP+si3D/uZ2G/+ri2////////////97Rx/99SiD/fUkf/31JH/99SR//fUkf/31KIP/KtqX//v7+/////////////////////////////////+Xb0/+HWDH/fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/6eFaP/l2tL//v7+////////////8evn/7yhi/+BTyb/fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/31JH/99SR//fUkf/4dXMP+Uakf/jV86/31JH/99SR//fUkf/31JH/99SR//fUkf/31JH/9/SyH/f0sh/39LIf9/SyH/f0sh/39LIf9/SyH/f0sh/39LIf9/SyH/f0sh/39LIf9/SyH/f0sh/39LIf9/SyH/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==";
 
@@ -61,6 +63,8 @@ public class PlanFragment extends Fragment {
         map_frame = root.findViewById(R.id.map_frame);
         robot_location = root.findViewById(R.id.robot_location);
         robot_margin = (FrameLayout.LayoutParams) robot_location.getLayoutParams();
+        robotView = new RobotView(root.getContext());
+        map_frame.addView(robotView);
 
         doRegisterReceiver();
         doRobotLocationReceiver();
@@ -100,8 +104,8 @@ public class PlanFragment extends Fragment {
                 } else {
                     if (checkDes(des)) {
                         robot.send_des(des);
-                        setRobotLocation(des);
-                        robot.setDes(des);
+                        //setRobotLocation(des);
+                        //robot.setDes(des);
                         Snackbar.make(v, "发送地址 " + des, Snackbar.LENGTH_SHORT).show();
                     } else {
                         Snackbar.make(v, "地址错误", Snackbar.LENGTH_SHORT).show();
@@ -176,8 +180,8 @@ public class PlanFragment extends Fragment {
     }
 
     private void setRobotLocation(String des)  {
-        int left = 160;
-        int top = 160;
+        int left = 0;
+        int top = 0;
         double x = 0;
         double y = 0;
         System.out.println("des--" + des);
@@ -186,10 +190,15 @@ public class PlanFragment extends Fragment {
             y = Double.valueOf(des.split(" ")[1]);
             left = 540 + (int) x*20*1080/map_width;
             top = 540 - (int) y*20*1080/map_height;
+            //left = 160 + (int) x*20*320/map_width;
+            //top = 160 - (int) y*20*320/map_height;
         }
         System.out.println("des--- " + x + " " + y);
         System.out.println("des--- " + left + " " + top);
-        robot_margin.setMargins(left, top, 0, 0);
-        robot_location.refreshDrawableState();
+        //robot_margin.setMargins(left, top, 0, 0);
+        //robot_location.refreshDrawableState();
+        robotView.setBitmapX(left);
+        robotView.setBitmapY(top);
+        robotView.invalidate();
     }
 }

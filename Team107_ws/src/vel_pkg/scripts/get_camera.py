fcc746d63f29012477e284aedfe2e2eaac87bc73
杨嘉成 <17373138@buaa.edu.cn>
2020-06-03 02:39:00 +0800

    position

--- Team107_ws/src/vel_pkg/scripts/get_camera.py
+++ Team107_ws/src/vel_pkg/scripts/get_camera.py
@@ -4,17 +4,62 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
- 
+import websocket
+from PIL import Image as PILImage
+import base64
+import io
+import json
+import time
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+def on_message(ws, message):
+    #print(ws)
+    print("reveive" + message)
 
 
+def on_error(ws, error):
+    #print(ws)
+    print(error)
+
+
+def on_close(ws):
+    #print(ws)
+    print("### closed ###")
+
+ws = websocket.WebSocketApp("ws://134.175.14.15:8080/demo/websocket/4",
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
+
+strEncode = ""
+oldStrEncode = ""
 def carama_callback(imgmsg):
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(imgmsg, "bgr8")
+
+    img = PILImage.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+    img = img.resize((150,150))
+    img = img.convert('L')
+    
+    #img.save('2.png','png')
+    
+    imgByteArr = io.BytesIO()
+    img.save(imgByteArr, format='PNG')
+    imgByteArr = imgByteArr.getvalue()
+    global strEncode
+    strEncode = base64.b64encode(imgByteArr)
+
+    print(strEncode)
+    
+    rospy.sleep(10.)
 #   add code here
-    print img
+#    ws.send()
 #   cv2.imwrite("1.png", img)
 #    cv2.waitKey(3)
- 
+    
 def carama_listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # node are launched, the previous one is kicked off. The
@@ -25,3 +70,21 @@ def carama_listener():
     rospy.Subscriber("/camera/rgb/image_raw", Image, carama_callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
+def on_open(ws):
+    def run(*args):
+        global oldStrEncode
+        global strEncode
+        while True:
+            time.sleep(5)
+            #print('send')
+            if strEncode != oldStrEncode:
+                s = '{"to":"android","from":"camera","type":"receive_camera","data":"'+strEncode+'"}'
+                oldStrEncode = strEncode
+                ws.send(s)
+    thread.start_new_thread(run, ())
+
+if __name__ == '__main__':
+    ws.on_open = on_open
+    thread.start_new_thread(ws.run_forever, ())
+    carama_listener()
---
+++ Team107_ws/src/vel_pkg/scripts/get_position.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+#coding=utf-8
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped,PoseStamped
+import websocket
+import time
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+def on_message(ws, message):
+    #print(ws)
+    print("reveive" + message)
+
+
+def on_error(ws, error):
+    #print(ws)
+    print(error)
+
+
+def on_close(ws):
+    #print(ws)
+    print("### closed ###")
+
+ws = websocket.WebSocketApp("ws://134.175.14.15:8080/demo/websocket/5",
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
+data = ""
+def PoseCallBack(msg):
+	#订阅到的坐标信息
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    global data
+    data = str(x)+' '+str(y)
+
+def PoseSub():
+	rospy.init_node('pose_sub',anonymous=False)
+	#监控话题，并在回调函数中处理
+	rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,PoseCallBack)
+	rospy.spin()
+
+
+
+def on_open(ws):
+    def run(*args):
+        global data
+        while True:
+            time.sleep(0.2)
+            print(data)
+            s = '{"to":"android","from":"pos","type":"receive_des","data":"'+data+'"}'
+            ws.send(s)
+    thread.start_new_thread(run, ())
+
+
+if __name__=='__main__':
+    ws.on_open = on_open
+    thread.start_new_thread(ws.run_forever, ())
+    PoseSub()
